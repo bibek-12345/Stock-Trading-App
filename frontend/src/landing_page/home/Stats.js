1f73e0c089c33482e663075854b638aa5d93ce07
Bibek Bhattarai <bibek.bhattarai072@gmail.com>
2025-12-17 17:41:55 -0500

    Navbar Done

--- frontend/src/landing_page/home/Stats.js
+++ frontend/src/landing_page/home/Stats.js
@@ -1,28 +1,42 @@
-import React from 'react';
+import React from "react";
 
 function Stats() {
-    return ( 
-        <div className='container p-3'>
-            <div className='row p-5'>
-                <div className='col-6 p-5'>
-                    <h1 className='fs-2 mb-5'>Trust with confidence</h1>
-                    <h2 className='fs-4'>Customer-first always</h2>
-                    <p className='text-muted'>That's why 1.3+ crore customers trust Zerodha with ₹3.5+ lakh crores worth of equity investments.</p>
-                    <h2 className='fs-4'>The Zerodha universe</h2>
-                    <p className='text-muted'>Not just an app, but a whole ecosystem. Our investments in 30+ fintech startups offer you tailored services specific to your needs.</p>
-                    <h2 className='fs-4'>Do better with money</h2>
-                    <p className='text-muted'>With initiatives like Nudge and Kill Switch, we don't just facilitate transactions, but actively help you do better with your money.</p>
-                </div>
-                <div className='col-6 p-5'>
-                    <img src='media\images\ecosystem.png' style={{width:"125%"}}></img>
-                    <div className='text-center'>
-                        <a href='' className='mx-5' style={{textDecoration: "none"}}>Explore our Products <i class="fa-solid fa-arrow-right-long"></i></a>
-                        <a href='' style={{textDecoration: "none"}}>Try Kite demo <i class="fa-solid fa-arrow-right-long"></i></a>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="container p-3">
+      <div className="row p-5">
+        <div className="col-6 p-5">
+          <h1 className="fs-2 mb-5">Trust with confidence</h1>
+          <h2 className="fs-4">Customer-first always</h2>
+          <p className="text-muted">
+            That's why 1.3+ crore customers trust Zerodha with ₹3.5+ lakh crores
+            worth of equity investments.
+          </p>
+          <h2 className="fs-4">The Zerodha universe</h2>
+          <p className="text-muted">
+            Not just an app, but a whole ecosystem. Our investments in 30+
+            fintech startups offer you tailored services specific to your needs.
+          </p>
+          <h2 className="fs-4">Do better with money</h2>
+          <p className="text-muted">
+            With initiatives like Nudge and Kill Switch, we don't just
+            facilitate transactions, but actively help you do better with your
+            money.
+          </p>
         </div>
-     );
+        <div className="col-6 p-5">
+          <img src="media/images/ecosystem.png" style={{ width: "125%" }}></img>
+          <div className="text-center">
+            <a href="" className="mx-5" style={{ textDecoration: "none" }}>
+              Explore our Products <i class="fa-solid fa-arrow-right-long"></i>
+            </a>
+            <a href="" style={{ textDecoration: "none" }}>
+              Try Kite demo <i class="fa-solid fa-arrow-right-long"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Stats;
