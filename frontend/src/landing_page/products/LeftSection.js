b36fae83094eac6eaeaa08133d7586c5768f6f25
Bibek Bhattarai <bibek.bhattarai072@gmail.com>
2025-12-18 09:55:54 -0500

    hero and leftsection done

--- frontend/src/landing_page/products/LeftSection.js
+++ frontend/src/landing_page/products/LeftSection.js
@@ -1,9 +1,47 @@
-import React from 'react';
+import React from "react";
 
-function LeftSection() {
-    return ( 
-        <h1>LeftSection</h1>
-    );
+function LeftSection({
+  imageURL,
+  productName,
+  productDescription,
+  tryDemo,
+  learnMore,
+  googlePlay,
+  appStore,
+}) {
+  return (
+    <div className="container mt-5 border border-info">
+      <div className="row p-5">
+        <div className="col-6">
+          <img src={imageURL}></img>
+        </div>
+        <div className="col-6 p-5 mt-5">
+          <h1>{productName}</h1>
+          <p>{productDescription}</p>
+          <div className="no-underline">
+            {tryDemo && (
+              <a href={tryDemo}>
+                Try Demo <i class="fa-solid fa-arrow-right-long"></i>
+              </a>
+            )}
+            {learnMore && (
+              <a href={learnMore} style={{ marginLeft: "50px" }}>
+                Learn More <i class="fa-solid fa-arrow-right-long"></i>
+              </a>
+            )}
+          </div>
+          <div className="mt-3">
+            <a href={googlePlay}>
+              <img src="media/images/googlePlayBadge.svg"></img>
+            </a>
+            <a href={appStore} style={{ marginLeft: "50px" }}>
+              <img src="media/images/appstoreBadge.svg"></img>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default LeftSection;
