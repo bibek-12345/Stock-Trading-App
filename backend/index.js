63bfe681bdd67d5f2d36a48f05574b49e00f37c2
Bibek Bhattarai <bibek.bhattarai072@gmail.com>
2025-12-23 14:59:48 -0500

    cors updated

--- backend/index.js
+++ backend/index.js
@@ -25,10 +25,14 @@ app.use(
       `https://stock-dashboard-p38l.onrender.com/`,
       "http://localhost:3000",
     ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
+app.options("*", cors(corsOptions));
 app.use(bodyParser.json());
+
 
 // Connect to MongoDB Atlas
 mongoose
